f7bbe2cb03acbbbd4e91b19c98abfd8e0b30ca47
Rajeshhossainmondal <130083005+Rajeshhossainmondal2001@users.noreply.github.com>
2023-08-25 11:50:59 +0530

    galleryopen

---
+++ app/src/main/java/com/example/galleryopen/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.galleryopen
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    val imagecode = 1
+    var imageUri: Uri? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        button = findViewById(R.id.buttonLoadPicture)
+        button.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, imagecode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == imagecode) {
+            imageUri = data?.data
+            imageView.setImageURI(imageUri)
+        }
+    }
+}
